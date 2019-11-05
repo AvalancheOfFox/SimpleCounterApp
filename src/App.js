8249f1aaf7fbe9b2dee27d9fe77bd84f71df52de
Ross Perkel <rossperkel@gmail.com>
2019-11-05 10:05:06 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Counter extends React.Component {
+
+  state = {
+    count: 0
+  }
+
+  increment(){
+    this.setState = {
+      count: this.state.count + 1
+    }
+  }
+  decrement(){
+    this.setState = {
+      count: this.state.count - 1
+    }
+  }
+  reset(){
+    this.setState = {
+      count: 0
+    }
+  }
+
+  render(){
+  return (
+    <div className="main">
+      <button id="increment" onClick={this.increment()}>Increment</button>
+      <button id="decrement" onClick={this.decrement()}>Decrement</button>
+      <button id="reset" onClick={this.reset()}>Reset</button>
+
+      <h1>{this.state.count}</h1>
+    </div>
+  );
+}
+}
+
+export default Counter;
